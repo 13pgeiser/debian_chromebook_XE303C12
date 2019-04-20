1f44108b7b1eef0dc8f102cad6a5a230b66bc6e5
Pascal Geiser <pgeiser@pgeiser.com>
2019-04-20 17:19:16 +0200

    Linux kernel 5.0.7

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-kernel_version=5.0.5
+kernel_version=5.0.7
 config=$kernel_version-ARCH
-rcn_patch=armv7-x8
+rcn_patch=armv7-x9
 patches="0009-media-s5p-mfc-fix-incorrect-bus-assignment-in-virtua.patch"
 
 mkdir -p exynos
