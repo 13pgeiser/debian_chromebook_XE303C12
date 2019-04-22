800ae05df7715d1a820703987ecb8cb5023cefd4
Pascal Geiser <pgeiser@pgeiser.com>
2019-04-22 16:50:01 +0200

    Docker: prune all dangling images at the end of the process.

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 docker rm -f xe303c12 || true
+docker system prune -f -a
 docker build -t xe303c12 .
 docker run -d --privileged --name xe303c12 xe303c12 sleep 28800
 docker cp configs xe303c12:/configs
 docker cp scripts xe303c12:/scripts
 docker exec -i xe303c12 bash scripts/build.sh
 docker cp xe303c12:/exynos/xe303c12.zip ./xe303c12.zip
-find .
 docker rm -f xe303c12 || true
-docker system prune -f
+docker system prune -f -a
+
