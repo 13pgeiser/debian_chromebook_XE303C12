e746559058e47e84c1fe51634fda537fe3e3caaa
Pascal Geiser <pgeiser@pgeiser.com>
2019-05-15 18:54:02 +0200

    Increase build timeout

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,7 +3,7 @@ set -e
 docker rm -f xe303c12 || true
 docker system prune -f -a
 docker build -t xe303c12 .
-docker run -d --privileged --name xe303c12 xe303c12 sleep 28800
+docker run -d --privileged --name xe303c12 xe303c12 sleep 43200
 docker cp configs xe303c12:/configs
 docker cp scripts xe303c12:/scripts
 docker exec -i xe303c12 bash scripts/build.sh
